9035b461ec73944be5ed497fcc1ae3e4b9d0d09b
Aarin2802 <40752701+Aarin2802@users.noreply.github.com>
2018-07-03 12:43:51 +0530

    to modify message

---
+++ source/Test.java
@@ -0,0 +1,13 @@
+
+
+
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+    System.out.println("whats up u shld enter ur number right here dude ");
+    int a=sc.nextInt();
+        System.out.println("+ a);
+    }
+}
